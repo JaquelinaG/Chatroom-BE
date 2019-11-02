7c42169a374ba7ed3fc60e81a91564ed3c6b1475
JaquelinaG <jaquelinagiuliani@gmail.com>
2019-11-02 20:45:04 -0300

    New API for chatroom. New Hub for the chat and configuration of SignalR in Startup.

---
+++ Chatroom.WebApi/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Chatroom.Domain;
+
+namespace Chatroom.WebApi
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            await this.Clients.All.SendAsync("shareMessage", message);
+        }
+    }
+}
