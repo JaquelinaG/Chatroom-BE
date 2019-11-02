87f8eebe88d3f3652809d9765cbc65e6ace0167e
JaquelinaG <jaquelinagiuliani@gmail.com>
2019-11-02 19:49:59 -0300

    Creation of Solution for server side app. Domain project with entity Message.

---
+++ Chatroom.Domain/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chatroom.Domain
+{
+    public class Message
+    {
+        public int ID { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Name { get; set; }
+    }
+}
